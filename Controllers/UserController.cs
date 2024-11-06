0bd706d8af26cca45256d88f7e9403848937f81c
Alex <alexander.christianson@outlook.com>
2024-11-06 15:50:42 +0100

    Created basic crud actions for UserController without finished views

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -19,6 +19,15 @@ namespace StoryPromptMVC.Controllers
             return View();
         }
 
+        public async Task<IActionResult> AdminUserHandler()
+        {
+            var response = await _client.GetAsync(baseAdress);
+            var content = await response.Content.ReadAsStringAsync();
+            var users = JsonConvert.DeserializeObject<List<UserVM>>(content);
+
+            return View(users);
+        }
+
         public IActionResult CreateUser()
         {
             return View();
@@ -40,7 +49,53 @@ namespace StoryPromptMVC.Controllers
                 return BadRequest();
             }
 
-            return RedirectToAction("CreateUser");
+            return RedirectToAction("Index");
         }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteUser(string userId)
+        {
+            if(userId == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var response = await _client.DeleteAsync($"{baseAdress}/{userId}");
+            if(!response.IsSuccessStatusCode)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> EditUser(string userId)
+        {
+            var response = await _client.GetAsync($"{baseAdress}/{userId}");
+            var json = await response.Content.ReadAsStringAsync();
+            var user = JsonConvert.DeserializeObject<UserVM>(json);
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUser(UserVM userToEdit)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var json = JsonConvert.SerializeObject(userToEdit);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _client.PutAsync($"{baseAdress}/{userToEdit.id}", content);
+            if(!response.IsSuccessStatusCode)
+            {
+                return BadRequest();
+            }
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
