47fb741bd30000c2a00cf626082c5e555ac0e89c
Ashfaqul Awal <ashfaq.awal@gmail.com>
2024-11-12 20:46:00 +0100

    Created Story Reaction View Model.

--- Models/StoryReaction/StoryReactionVM.cs
+++ Models/StoryReaction/StoryReactionVM.cs
@@ -2,5 +2,10 @@
 {
     public class StoryReactionVM
     {
+        public int Id { get; set; }
+        public string Reaction { get; set; }
+        public int StoryId { get; set; }
+        public string? UserId { get; set; }
+
     }
 }
