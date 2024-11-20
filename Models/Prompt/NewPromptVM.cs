28c4d4b1e8715cf8cd02afb3720888928d3ddec2
Alex <alexander.christianson@outlook.com>
2024-11-20 15:23:13 +0100

    Started with view for Top and new prompts

---
+++ Models/Prompt/NewPromptVM.cs
@@ -0,0 +1,13 @@
+﻿using StoryPromptMVC.Models.User;
+
+namespace StoryPromptMVC.Models.Prompt
+{
+    public class NewPromptVM
+    {
+        public int Id { get; set; }
+        public string PromptContent { get; set; }
+        public DateTime PromptDateCreated { get; set; }
+        public UserPromptVM user { get; set; }
+        public int ReactionCount { get; set; }
+    }
+}
